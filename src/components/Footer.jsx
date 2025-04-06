8780406febf3ab23ce2685132ef5bb8ab956d46c
Atunwa Vincent <atunwavincent1999@gmail.com>
2025-04-06 22:38:20 +0100

    Responsivness for footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,29 +2,29 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-[150px]">
+    <footer className="w-full pt-[90px] md:pt-[150px]">
       <div className="bg-[#072a30] w-full h-fit pb-[60px]">
         <div className="text-white text-[17px] tracking-[-1px] flex flex-col gap-[20px] pt-[100px] ">
-          <div className="w-full flex justify-between items-center px-[80px]">
+          <div className="w-full gap-[50px] md:gap-0 flex flex-col px-[24px] md:flex-row md:justify-between md:items-center md:px-[50px] lg:px-[80px] ">
             {/* Navs */}
-            <div className="flex flex-col gap-[30px] ">
-              <h4>Legal</h4>
-              <div className="flex gap-[15px]">
+            <div className="flex flex-col gap-[15px] md:gap-[30px]">
+              <h4 className="!text-[21px]">Legal</h4>
+              <div className="flex !flex-row md:!flex-col gap-[18px] 2xl:!flex-row 2xl:gap-[15px]">
                 <p className="underline cursor-pointer">Privacy policies</p>
                 <p className="underline cursor-pointer">Terms and conditions</p>
               </div>
             </div>
-            <div className="">
-              <div className="flex gap-[30px]">
+            <div className="w-full md:w-fit">
+              <div className="flex flex-col gap-[15px] md:flex-row md:gap-[20px] lg:gap-[30px]">
                 <p className="underline cursor-pointer">Kriacard</p>
                 <p className="underline cursor-pointer">Company</p>
                 <p className="underline cursor-pointer">Ask Kriapay</p>
                 <p className="underline cursor-pointer">Login</p>
               </div>
             </div>
-            <div className="flex flex-col gap-[30px]">
-              <h4>Connect</h4>
-              <div className="flex gap-[15px]">
+            <div className="flex flex-col gap-[15px] md:gap-[30px]">
+              <h4 className="!text-[21px]">Connect</h4>
+              <div className="flex !flex-row md:!flex-col gap-[18px] 2xl:!flex-row 2xl:gap-[15px]">
                 <div className="flex gap-[10px]">
                   <img
                     className="w-[22px] h-[22px] object-cover "
@@ -46,17 +46,16 @@ const Footer = () => {
                     className="w-[22px] h-[22px] object-cover "
                     src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553804/Instagram_aeil0t.svg"
                     alt="Kriapay Instagram"
-                  />
+                  /> Instagram
                 </div>
-                <p className="underline cursor-pointer">Instagram</p>
               </div>
             </div>
           </div>
 
           {/* Kriapay logo */}
-          <div className="w-full px-[80px] mt-[100px] ">
+          <div className="w-full px-[24px] md:px-[50px] lg:px-[80px] mt-[80px] md:mt-[100px] ">
             <img
-              className="w-[137px] h-[50px] "
+              className="w-[110px] h-[40px] md:w-[137px] md:h-[50px] "
               src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1741440756/Kriapay_Logo_nm8w7f.svg"
               alt="Kriapay logo"
             />
@@ -64,9 +63,9 @@ const Footer = () => {
 
           {/* Company details & Exchange rate */}
 
-          <div className="w-full mt-[25px] flex justify-between items-center px-[80px] ">
-            <div className="w-[50%] flex flex-col gap-[10px]">
-              <div className="">
+          <div className="w-full mt-[25px] flex flex-col md:flex-row justify-between items-center px-[24px] md:px-[50px] lg:px-[80px] ">
+            <div className="w-full md:w-[50%] flex flex-col gap-[10px]">
+              <div className="w-full">
                 <span className="font-semibold mr-1">
                   Kria Technologies Ltd.
                 </span>
@@ -74,7 +73,8 @@ const Footer = () => {
                 and operates under regulatory frameworks through trusted
                 financial partners across regions.
               </div>
-              <div className="pt-[20px] flex gap-[10px] ">
+              <div className="pt-[20px] md:hidden ">All rights reserved. © 2025.</div>
+              <div className="pt-[30px] md:pt-[20px] flex gap-[10px] w-full">
                 <img
                   className="w-[25px] h-[25px] "
                   src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553804/Location_pin_qefazd.svg"
@@ -83,7 +83,7 @@ const Footer = () => {
                 Onijanikin Street, Alasia, Lagos, Nigeria. - Not open to the
                 public.
               </div>
-              <div className="flex gap-[10px]">
+              <div className="flex pt-[8px] md:pt-0 gap-[10px] w-full">
                 <img
                   className="w-[25px] h-[25px] "
                   src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742508820/mail-alt-3-svgrepo-com_1_b8rfqw.svg"
@@ -91,9 +91,9 @@ const Footer = () => {
                 />
                 info@kriapay.com
               </div>
-              <div className="pt-[20px] ">All rights reserved. © 2025.</div>
+              <div className="md:pt-[30px] lg:pt-[20px] hidden md:block ">All rights reserved. © 2025.</div>
             </div>
-            <div className="w-[50%] flex justify-end">
+            <div className="w-full pt-[58px] md:pt-0 md:w-[50%] flex justify-center md:justify-end">
               <p className="">Exchange rates may vary by market.</p>
             </div>
           </div>
