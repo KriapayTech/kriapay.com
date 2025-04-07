ba0ed5e5669dc106091a72df2c23a5c9c52a44e9
Atunwa Vincent <atunwavincent1999@gmail.com>
2025-04-07 23:01:26 +0100

    Responsiveness for the Setup Kriacard done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full pt-[90px] md:pt-[150px]">
       <div className="bg-[#072a30] w-full h-fit pb-[60px]">
-        <div className="text-white text-[15px] md:text-[17px] tracking-[-1px] leading-[32px] lg:leading-[33px] flex flex-col gap-[20px] pt-[100px] ">
+        <div className="text-white text-[15px] md:text-[17px] tracking-[-1px] leading-[32px] lg:leading-[34px] flex flex-col gap-[20px] pt-[100px] ">
           <div className="w-full gap-[50px] md:gap-0 flex flex-col px-[24px] md:flex-row md:justify-between md:items-center md:px-[50px] lg:px-[80px] ">
             {/* Navs */}
             <div className="flex flex-col gap-[15px] md:gap-[30px]">
@@ -25,7 +25,7 @@ const Footer = () => {
             <div className="flex flex-col gap-[15px] md:gap-[30px]">
               <h4 className="!text-[21px]">Connect</h4>
               <div className="flex !flex-row md:!flex-col gap-[18px] 2xl:!flex-row 2xl:gap-[15px]">
-                <div className="flex gap-[10px]">
+                <div className="flex gap-[10px] leading-[20px] ">
                   <img
                     className="w-[22px] h-[22px] object-cover "
                     src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553795/FaceBook_fwhfmj.svg"
@@ -33,7 +33,7 @@ const Footer = () => {
                   />
                   <p className="underline cursor-pointer">Facebook</p>
                 </div>
-                <div className="flex gap-[10px]">
+                <div className="flex gap-[10px]  leading-[20px]">
                   <img
                     className="w-[22px] h-[22px] object-cover "
                     src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553806/X_or_Twitter_drnhcz.svg"
@@ -41,7 +41,7 @@ const Footer = () => {
                   />
                   X
                 </div>
-                <div className="flex gap-[10px]">
+                <div className="flex gap-[10px]  leading-[20px]">
                   <img
                     className="w-[22px] h-[22px] object-cover "
                     src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553804/Instagram_aeil0t.svg"
@@ -74,7 +74,7 @@ const Footer = () => {
                 financial partners across regions.
               </div>
               <div className="pt-[20px] md:hidden ">All rights reserved. © 2025.</div>
-              <div className="pt-[30px] md:pt-[20px] flex gap-[10px] w-full">
+              <div className="pt-[30px] md:pt-[20px] flex gap-[10px] w-full  leading-[22px]">
                 <img
                   className="w-[25px] h-[25px] "
                   src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553804/Location_pin_qefazd.svg"
@@ -83,7 +83,7 @@ const Footer = () => {
                 Onijanikin Street, Alasia, Lagos, Nigeria. - Not open to the
                 public.
               </div>
-              <div className="flex pt-[8px] md:pt-0 gap-[10px] w-full">
+              <div className="flex pt-[14px] md:pt-[14px] gap-[10px] w-full leading-[22px]">
                 <img
                   className="w-[25px] h-[25px] "
                   src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742508820/mail-alt-3-svgrepo-com_1_b8rfqw.svg"
--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -4,20 +4,21 @@ import { Button } from "react-bootstrap";
 const Setup = () => {
   return (
     <div className="bg-[#FFFFFF] overflow-hidden mt-[150px]">
-      <div className="flex justify-between items-center px-[100px] gap-[20px] mt-[50px]">
-        <div className="overflow-hidden relative h-[500px]  w-[50%]">
-          <img className="absolute w-[800px] h-[870px] right-[-30px] top-[-30px] object-cover "
+      <div className="flex flex-col lg:flex-row justify-between items-center w-full md:px-[80px] 2xl:px-[100px] gap-[20px]  2xl:gap-[20px] lg:mt-[50px]  ">
+        <div className="overflow-hidden  px-[30px] md:px-0 relative h-[350px] lg:h-[450px] 2xl:h-[500px] w-full lg:w-[54%]  ">
+          <img
+            className="absolute w-full md:w-[550px] lg:w-[650px] 2xl:w-[800px] lg:h-[670px] 2xl:h-[870px] left-[20px] md:left-[0px] lg:left-[-20px] bottom-[-100px] md:top-[-30px] object-cover   "
             src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742301448/Kriacard_black_a9kwl7.svg"
             alt="Kriapay Kriacard"
           />
         </div>
-        <div className="w-[42%] h-fit items-end flex flex-col gap-[50px] pl-[10px] pb-[110px] ">
-          <p className="text-left w-full  text-[#072A30] text-[28px] tracking-[-1.5px] leading-[33px] font-medium   ">
+        <div className="w-full px-[30px] md:px-[20px] lg:px-0 lg:w-[45%]  pt-[60px] lg:pt-0 2xl:w-[42%] h-fit   items-start lg:items-end flex flex-col gap-[30px] lg:gap-[50px]  2xl:pl-[0px] md:pr-[20px] 2xl:pr-[33px] pb-[30px] lg:pb-[110px] ">
+          <p className="text-left w-[80%] flex lg:w-full  text-[#072A30] text-[23px] md:text-[27px] lg:text-[28px] tracking-[-1.5px] leading-[33px] font-medium   ">
             Use Kriacard anywhere Visa or Verve is accepted
           </p>
-          <p className="text-left w-full  text-[#072A30] text-[21px] tracking-[-1.5px] leading-[30px] font-normal  ">
+          <p className="text-left w-[90%] lg:w-full  text-[#072A30] text-[15px] md:text-[21px] tracking-[-1px] md:tracking-[-1.5px] leading-[30px] font-normal  ">
             Shop online, subscribe to services like Netflix and Spotify, and
-            make payments on international platforms with no hidden fees and 
+            make payments on international platforms with no hidden fees and
             currency barriers.
           </p>
         </div>
