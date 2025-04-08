cfad0f964a87ab1ed5317ba8039bf8e9869397c6
Atunwa Vincent <atunwavincent1999@gmail.com>
2025-04-08 16:14:15 +0100

    Company hero responsiveness done

--- src/components/Companyhero.jsx
+++ src/components/Companyhero.jsx
@@ -7,23 +7,35 @@ const Hero = () => {
     <div className=" bg-[#072a30] overflow-hidden w-full ">
       {/* Text Section */}
       <div className="text-white flex flex-col items-center  pt-[30px]">
-        <div className=" text-center flex flex-col gap-[40px] pt-[30px] ">
-          <h1 className="!text-[44px] !font-medium tracking-[-1.5px] leading-[48px]  ">
-            Redefining Financial Connectivity <br /> for Africa
+        <div className="text-center flex flex-col gap-[40px] pt-[30px] w-[85%] md:w-[70%] lg:w-[75%] 2xl:w-full">
+          <h1 className="!text-[26px] md:!text-[30px] lg:!text-[40px] !font-medium tracking-[-1.5px] !leading-[33px] md:!leading-[39px] lg:!leading-[45px] 2xl:!leading-[48px]">
+            Redefining Financial Connectivity
+            <span className="hidden md:inline">
+              <br />
+            </span>{" "}
+            for Africa
           </h1>
-          <p className="text-[18px] font-normal opacity-90 tracking-[-0.5px] leading-[28px] ">
+
+          <p className="text-[14px] md:text-[16px] lg:text-[18px] z-20 font-normal tracking-[-0.6px] md:tracking-[-0.7px] opacity-90 ">
             We are pioneers of borderless financial access for Africans, with a
-            vision to <br /> facilitate financial transactions that are
-            frictionless, inclusive, and accessible
-            <br /> to all.
+            vision to
+            <span className="hidden lg:inline">
+              <br />
+            </span>
+            facilitate financial transactions that are frictionless, inclusive,
+            and accessible
+            <span className="hidden lg:inline">
+              <br />
+            </span>
+            to all.
           </p>
         </div>
 
-        <div className="w-full h-[320px] flex items-center justify-center relative">
+        <div className="w-full h-[270px] md:h-[300px] lg:h-[320px] flex items-center justify-center relative">
           <img
             src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742306015/Company_coins_ldrnrh.svg"
             alt="Kriapay company"
-            className="rotate-[7deg] w-[41%] absolute bottom-[-120px] "
+            className="rotate-[7deg] w-[85%] md:w-[50%] lg:w-[40%] 2xl:w-[38%] absolute bottom-[-55px] md:bottom-[-70px] lg:bottom-[-85px] 2xl:bottom-[-115px] "
           />
         </div>
       </div>
