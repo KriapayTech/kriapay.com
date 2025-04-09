9458686cbe20a45e23524e9ecddf93e3a0bdbed7
Atunwa Vincent <atunwavincent1999@gmail.com>
2025-04-09 15:13:41 +0100

    Desktop and tab responsiveness for the Create slider done

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const slidesData = [
   {
     title: "Visit https://kriapay.com and navigate to Signup",
-    subTitle: "The following details are needed",
+    subTitle: "The following details are needed:",
     step1: "• Full name",
     step2: "• Date of birth",
     step3: "• Email address",
@@ -13,20 +13,21 @@ const slidesData = [
   },
   {
     title: "KYC Verification",
+    subTitle: "The required documents for profile verification:",
     step1:
-      "Government issued ID (National ID, Passport, Driver’s license, etc.) Tier 1",
-    step2: "Proof of address (Utility bill or bank statement) Tier 2",
-    step3: "Proof of funds Tier 3",
+      "• Government issued ID (National ID, Passport, Driver’s license, etc.) - Tier 2",
+    step2: "• Proof of address (Utility bill or bank statement) - Tier 2",
+    step3: "• Proof of funds - Tier 3",
     step4:
       "These documents are verified against the information provided during signup.",
   },
   {
     title: "Protecting Your Account & Transactions",
-    step1: "Use a strong, unique password to keep your account secure.",
+    step1: "• Use a strong, unique password to keep your account secure.",
     step2:
-      "Create a secure transaction PIN for quick and safe payments during signup",
+      "• Create a secure transaction PIN for quick and safe payments during signup",
     step3:
-      "If you forget your password or PIN, you can easily reset them in-app with a few simple steps.",
+      "• If you forget your password or PIN, you can easily reset them in-app with a few simple steps.",
     step4:
       "Never share your password or PIN. Always reset them immediately if you suspect any unauthorized access.",
   },
@@ -34,67 +35,96 @@ const slidesData = [
 
 const Create = () => {
   return (
-    <div className="w-full flex justify-center mt-[70px] md:mt-[100px] lg:mt-[150px] border-2 border-red-500">
-      <div className="w-[85%] bg-[#072a30] rounded-[20px] flex flex-col items-center text-center mt-[100px] mb-[100px] md:pb-[20px] lg:pb-[10px] 2xl:pb-[20px] relative overflow-hidden">
+    <div className="w-full flex justify-center mt-[70px] md:mt-[100px] lg:mt-[150px]">
+      <div className="w-[85%] bg-[#072a30] rounded-[20px] flex flex-col items-center text-center mt-[100px] mb-[100px] md:pb-[20px] lg:pb-[50px] 2xl:pb-[20px] relative overflow-hidden">
         <p className="text-[30px] md:text-[33px] lg:text-[39px] 2xl:text-[40px] text-white pt-[60px] tracking-[-3px] font-medium  ">
           Creating an account{" "}
         </p>
-        
 
-        <div className="hidden md:block w-[100%] md:h-[450px] lg:h-[530px] mt-16 md:mt-12 2xl:mt-4 ">
+        <div className="hidden md:block w-[100%] md:h-[620px] lg:h-[590px] 2xl:h-[530px] mt-16 md:mt-12 2xl:mt-4 ">
           <div
             id="currencyCarousel"
             className="carousel slide"
             data-bs-ride="carousel"
           >
-            
             <div className="carousel-inner overflow-visible ">
               {slidesData.map((slide, index) => (
                 <div
-                  className={`carousel-item ${index === 0 ? "active" : ""}`} 
+                  className={`carousel-item ${index === 0 ? "active" : ""}`}
                   key={index}
                 >
                   <div className="flex-row flex align-items-center text-left w-full overflow-visible ">
-                    <div className="col-md-6 text-white  !w-[49%] !pl-[120px] pt-[20px] ">
+                    <div className="col-md-6 text-white !w-[65%] lg:!w-[51%] 2xl:!w-[49%] pl-[55px] lg:pl-[80px] 2xl:!pl-[120px] pt-[20px]">
                       <h2
-                        className={`flex items-center gap-2 ${
+                        className={`flex items-center  text-left gap-2 ${
                           index === 0
-                            ? "!text-[18px] tracking-[-0.6px] leading-[25px] !font-normal"
-                            : "text-[21px] font-semibold"
+                            ? "md:!text-[17px] lg:!text-[18px] tracking-[-0.6px] leading-[25px] !font-normal"
+                            : "md:!text-[26px] lg:!text-[27px] !font-medium tracking-[-2px] leading-[25px] "
                         }`}
                       >
                         <img
                           src="https://res.cloudinary.com/dhap3isd2/image/upload/v1744198707/Kriapay%20website/qtifdmgqsmyoxsrysdnb.svg"
                           alt="Kriapay coin"
-                          className="w-[50px] h-[50px]"
+                          className="md:w-[45px] md:h-[45px] lg:w-[50px] lg:h-[50px]"
                         />
 
                         {index === 0 ? (
                           <>
-                            Visit https://kriapay.com and navigate to{" "}
-                            <span className="font-semibold">Signup</span>
+                            Visit https://kriapay.com and navigate to {" "}
+                            <span className="font-semibold ">Signup</span>
                           </>
                         ) : (
                           slide.title
                         )}
                       </h2>
                       <div className="w-full pl-[15px]">
-                      <h4 className="!text-[21px] tracking-[-1px] leading-[31px] pt-[30px] ">{slide.subTitle} </h4>
-                      <p className="text-[18px] tracking-[-0.5px] leading-[31px] pt-[20px]">{slide.step1}</p>
-                      <p className="text-[18px] tracking-[-0.5px] leading-[31px]">{slide.step3}</p>
-                      <p className="text-[18px] tracking-[-0.5px] leading-[31px] ">{slide.step2}</p>
-                      <p className="text-[18px] tracking-[-0.5px] leading-[31px]">{slide.step4}</p>
-                      <p className="text-[18px] tracking-[-0.5px] leading-[31px]">{slide.step5}</p>
-                      <p className="text-[18px] tracking-[-0.5px] leading-[31px]">{slide.step6}</p>
-                      <p className="text-[18px] tracking-[-0.5px] leading-[31px]">{slide.step7}</p>
+                        {index !== 2 && slide.subTitle && (
+                          <h4 className="md:!text-[20px] lg:!text-[21px] tracking-[-1px] leading-[32px] pt-[30px]">
+                            {slide.subTitle}
+                          </h4>
+                        )}
+
+                        <p
+                          className={`!text-[17px] lg:text-[18px] tracking-[-0.5px] leading-[32px] ${
+                            index === 2 ? "pt-[45px]" : "pt-[20px]"
+                          }`}
+                        >
+                          {slide.step1}
+                        </p>
+
+                        <p className="!text-[17px] lg:text-[18px] tracking-[-0.5px] leading-[32px]">
+                          {slide.step2}
+                        </p>
+                        <p className="!text-[17px] lg:text-[18px] tracking-[-0.5px] leading-[32px] ">
+                          {slide.step3}
+                        </p>
+                        <p
+                          className={`!text-[17px] lg:text-[18px] tracking-[-0.5px] leading-[32px] ${
+                            index === 0 ? "pt-[0px]" : "pt-[40px]"
+                          }`}
+                        >
+                          {slide.step4}
+                        </p>
+
+                        <p className="!text-[17px] lg:text-[18px] tracking-[-0.5px] leading-[32px]">
+                          {slide.step5}
+                        </p>
+                        <p className="!text-[17px] lg:text-[18px] tracking-[-0.5px] leading-[32px]">
+                          {slide.step6}
+                        </p>
+                        <p className="!text-[17px] lg:text-[18px] tracking-[-0.5px] leading-[32px]">
+                          {slide.step7}
+                        </p>
                       </div>
-                      
                     </div>
                   </div>
-                  
                 </div>
               ))}
-              <img className="w-[600px] h-[600px] absolute right-0 bottom-[-200px] opacity-25 " src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1741523154/Abuja_Landmark_1_exzagr.svg" alt="Kriapay landmark" />
+              <img
+                className="md:w-[450px] lg:w-[550px] 2xl:w-[600px] 2xl:h-[600px] absolute right-0 bottom-[-200px] opacity-25 "
+                src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1741523154/Abuja_Landmark_1_exzagr.svg"
+                alt="Kriapay landmark"
+              />
             </div>
             {/* Carousel Controls desktop and tab*/}
             <button
