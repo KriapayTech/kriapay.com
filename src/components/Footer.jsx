7575346eca2d85ab3a006465b1612f7ef3ce9ced
Atunwa Vincent <atunwavincent1999@gmail.com>
2025-04-10 09:29:24 +0100

    Included all links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,37 +16,71 @@ const Footer = () => {
             </div>
             <div className="w-full md:w-fit">
               <div className="flex flex-col gap-[15px] md:flex-row md:gap-[20px] lg:gap-[30px]">
-                <p className="underline cursor-pointer">Kriacard</p>
-                <p className="underline cursor-pointer">Company</p>
-                <p className="underline cursor-pointer">Ask Kriapay</p>
-                <p className="underline cursor-pointer">Login</p>
+                <a href="/kriacard">
+                  <p className="underline cursor-pointer text-white">
+                    Kriacard
+                  </p>
+                </a>
+                <a href="/Company">
+                  <p className="underline cursor-pointer text-white">Company</p>
+                </a>
+                <a href="/Ask">
+                  <p className="underline cursor-pointer text-white">
+                    Ask Kriapay
+                  </p>
+                </a>
+                <a
+                  href="https://kriapay.vercel.app/login"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p className="underline cursor-pointer text-white">Login</p>
+                </a>
               </div>
             </div>
             <div className="flex flex-col gap-[15px] md:gap-[30px]">
               <h4 className="!text-[20px] md:!text-[21px]">Connect</h4>
               <div className="flex !flex-row md:!flex-col gap-[18px] 2xl:!flex-row 2xl:gap-[15px]">
-                <div className="flex gap-[10px] leading-[20px] ">
-                  <img
-                    className="w-[22px] h-[22px] object-cover "
-                    src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553795/FaceBook_fwhfmj.svg"
-                    alt="Kriapay facebook"
-                  />
-                  <p className="underline cursor-pointer">Facebook</p>
+                <div className="flex leading-[20px] ">
+                  <a
+                    href="https://web.facebook.com/people/Kriapay/61561505605855/?_rdc=1&_rdr#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex gap-[10px] text-white"
+                  >
+                    <img
+                      className="w-[22px] h-[22px] object-cover "
+                      src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553795/FaceBook_fwhfmj.svg"
+                      alt="Kriapay facebook"
+                    />
+                    <p className="underline cursor-pointer">Facebook</p>
+                  </a>
+                </div>
+                <div className="flex leading-[20px]">
+                  <a
+                    href="https://x.com/kriapay"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex gap-[10px] text-white"
+                  >
+                    <img
+                      className="w-[22px] h-[22px] object-cover "
+                      src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553806/X_or_Twitter_drnhcz.svg"
+                      alt="Kriapay twitter"
+                    />
+                    X
+                  </a>
                 </div>
                 <div className="flex gap-[10px]  leading-[20px]">
-                  <img
-                    className="w-[22px] h-[22px] object-cover "
-                    src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553806/X_or_Twitter_drnhcz.svg"
-                    alt="Kriapay twitter"
-                  />
-                  X
-                </div>
-                <div className="flex gap-[10px]  leading-[20px]">
+                  <a href="https://www.instagram.com/kriapay_ng/" target="_blank" rel="noopener noreferrer" className="flex gap-[10px] text-white">
                   <img
                     className="w-[22px] h-[22px] object-cover "
                     src="https://res.cloudinary.com/dw7w2at8k/image/upload/v1742553804/Instagram_aeil0t.svg"
                     alt="Kriapay Instagram"
-                  /> Instagram
+                  />{" "}
+                  Instagram
+                  </a>
+                  
                 </div>
               </div>
             </div>
@@ -73,7 +107,9 @@ const Footer = () => {
                 and operates under regulatory frameworks through trusted
                 financial partners across regions.
               </div>
-              <div className="pt-[20px] md:hidden ">All rights reserved. © 2025.</div>
+              <div className="pt-[20px] md:hidden ">
+                All rights reserved. © 2025.
+              </div>
               <div className="pt-[30px] md:pt-[20px] flex gap-[10px] w-full  leading-[22px]">
                 <img
                   className="w-[25px] h-[25px] "
@@ -91,7 +127,9 @@ const Footer = () => {
                 />
                 info@kriapay.com
               </div>
-              <div className="md:pt-[30px] lg:pt-[20px] hidden md:block ">All rights reserved. © 2025.</div>
+              <div className="md:pt-[30px] lg:pt-[20px] hidden md:block ">
+                All rights reserved. © 2025.
+              </div>
             </div>
             <div className="w-full pt-[58px] md:pt-0 md:w-[50%] flex justify-center md:justify-end">
               <p className="">Exchange rates may vary by market.</p>
