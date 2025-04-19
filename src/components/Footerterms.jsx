bd9cf4068c3dd81d77a41b9b52148bbb6c47a1eb
Atunwa Vincent <atunwavincent1999@gmail.com>
2025-04-19 23:08:22 +0100

    Terms and Conditions for desktop done

--- src/components/Footerterms.jsx
+++ src/components/Footerterms.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Footer = () => {
+const Footerterms = () => {
   return (
-    <footer className="w-full pt-[90px] md:pt-[150px] ">
+    <footer className="w-full">
       <div className="bg-[#072a30] w-full h-fit pb-[60px]">
         <div className="text-white text-[15px] md:text-[16px] 2xl:text-[17px] tracking-[-1px] leading-[32px] lg:leading-[34px] flex flex-col gap-[20px] pt-[100px] ">
           <div className="w-full gap-[50px] md:gap-0 flex flex-col px-[24px] md:flex-row md:justify-between md:items-center md:px-[50px] lg:px-[80px] ">
@@ -144,4 +144,4 @@ const Footer = () => {
   );
 };
 
-export default Footer;
+export default Footerterms;
--- src/pages/Termsandconditions.jsx
+++ src/pages/Termsandconditions.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ConditionsHero from "../components/ConditionsHero";
 import Navigation from "../components/Navbar";
-
-import Footer from "../components/Footer";
+import ConditionsBody from "../components/ConditionsBody";
+import Footerterms from "../components/Footerterms";
 
 const Termsandconditions = () => {
   return (
     <div className="div-fluid">
       <Navigation />
       <ConditionsHero />
+      <ConditionsBody />
 
-      <Footer />
+      <Footerterms />
     </div>
   );
 };
